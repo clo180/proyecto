5cff54aff6dd60cceeade455a00d3b335a5655ec
clo180 <clotilde.delembeye@edu.ece.fr>
2023-04-11 10:04:59 +0200

    Ajout interface graphique

---
+++ src/main/java/org/example/HelloController.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import java.io.IOException;
+
+public class HelloController {
+    @FXML
+    private void handleButtonAction(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("page-invite.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.setScene(scene);
+        stage.show();
+    }
+}
